2fa58887e2a5d059f21fd43f0c2fe503dd0901e7
zhangmaoquan <hegongdajava@163.com>
2018-11-19 09:16:24 +0800

    第五次

--- src/main/java/mapper/Test.java
+++ src/main/java/mapper/Test.java
@@ -2,6 +2,6 @@ package mapper;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("是不我可看是发射点犯得上大三大四");
+        System.out.println("是不我可看是发射大苏打点大苏打撒旦犯得上大三大四");
     }
 }
